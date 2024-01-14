3d7ac71ca4131f9b35a39d6049d7fa2eee755910
TomP6 <thomas.p@frc1073.org>
2024-01-14 14:32:05 -0500

    added imported file

---
+++ SwerveDriveBase-imported/src/main/java/frc/robot/commands/ParkingBrake.java
@@ -0,0 +1,44 @@
+package frc.robot.commands;
+
+import edu.wpi.first.wpilibj2.command.Command;
+import frc.robot.subsystems.DriveSubsystem;
+
+public class ParkingBrake extends Command
+{
+    DriveSubsystem drivetrain;
+    boolean releaseOnEnd;
+
+
+    public ParkingBrake(DriveSubsystem ds, boolean releaseOnEnd)
+    {
+        this.drivetrain = ds;
+        this.releaseOnEnd = releaseOnEnd;
+        addRequirements(ds);
+    }
+
+    @Override
+    public void initialize()
+    {
+        drivetrain.parkingBrake(true); // turns on the parking brake
+    }
+
+    @Override
+    public void execute()
+    {
+    
+    }
+
+    @Override
+    public void end(boolean interrupted)
+    {
+        if (releaseOnEnd == true){
+        drivetrain.parkingBrake(false); // turns off the parking brake
+        }
+    }
+
+    @Override
+    public boolean isFinished()
+    {
+        return false;
+    }
+}
